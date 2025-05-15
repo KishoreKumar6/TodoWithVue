7f2a93404fdba2e715bf2a5137e554ace39c05a6
kishorekumar6 <kishoreramu20@gmail.com>
2025-05-15 19:05:03 +0530

    Routes

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
-import taskRoutes from "./routes/tasks.js";
+import taskRoutes from "./Routes/tasks.js";
 import dotenv from "dotenv";
 
 dotenv.config();
